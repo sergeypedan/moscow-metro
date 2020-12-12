b3b829c30895c228480ff16df7cf2fe8e54ca69d
Sergey Pedan <sergey.pedan@gmail.com>
2020-12-13 00:35:03 +0700

    Renames calls

--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -5,15 +5,15 @@ RSpec.describe MoscowMetro::Line do
 	subject { described_class.all }
 
 	it "All lines have names" do
-		expect(subject.find { |station| [nil, ""].include? station.name }).to eq nil
+		expect(subject.select { |station| [nil, ""].include? station.name }).to eq []
 	end
 
 	it "All lines have color" do
-		expect(subject.find { |station| [nil, ""].include? station.color }).to eq nil
+		expect(subject.select { |station| [nil, ""].include? station.color }).to eq []
 	end
 
 	it "All lines have UIDs" do
-		expect(subject.find { |station| [nil, ""].include? station.uid }).to eq nil
+		expect(subject.select { |station| [nil, ""].include? station.uid }).to eq []
 	end
 
 end
